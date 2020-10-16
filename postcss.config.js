91db5e48d6431db669976e7cf7831dccf83b5ade
Guillaume Martigny <guillaume.martigny@gmail.com>
2020-10-16 16:28:13 +0200

    First version for yasmina

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ process.env.NODE_ENV = "production";
 module.exports = {
     plugins: [
         require("@fullhuman/postcss-purgecss")({
-            content: ["./index.html"],
+            content: ["./**/*.html"],
         }),
         require("cssnano"),
     ],
