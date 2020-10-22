d74857c5789c8072f0acf86a6842d0c842f91823
Guillaume Martigny <guillaume.martigny@gmail.com>
2020-10-22 13:26:05 +0200

    Remove sass et postcss

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-process.env.NODE_ENV = "production";
-module.exports = {
-    plugins: [
-        require("@fullhuman/postcss-purgecss")({
-            content: ["./**/*.html"],
-        }),
-        require("cssnano"),
-    ],
-};
